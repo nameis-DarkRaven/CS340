e485c07c86495a91149ce54b66be4ca909202f4d
= <emileeforbush@gmail.com>
2026-02-17 16:39:26 -0700

    Everything up to Milestone 1

---
+++ tweeter-web-starter/tweeter-web/src/components/mainLayout/NavigationHooks.tsx
@@ -0,0 +1,46 @@
+import { AuthToken, User, FakeData } from "tweeter-shared";
+import { useMessageActions } from "../toaster/MessageHooks";
+import { useUserInfo, useUserInfoActions } from "../userInfo/UserInfoHooks";
+import { useNavigate } from "react-router-dom";
+
+export const useUserNavigation = () => {
+  const { displayErrorMessage } = useMessageActions();
+  const { displayedUser, authToken } = useUserInfo();
+  const { setDisplayedUser } = useUserInfoActions();
+  const navigate = useNavigate();
+
+  const extractAlias = (value: string): string => {
+    const index = value.indexOf("@");
+    return value.substring(index);
+  };
+
+  const getUser = async (
+    authToken: AuthToken,
+    alias: string,
+  ): Promise<User | null> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.findUserByAlias(alias);
+  };
+  const navigateToUser = async (
+    event: React.MouseEvent,
+    featurePath: string,
+  ): Promise<void> => {
+    event.preventDefault();
+
+    try {
+      const alias = extractAlias(event.target.toString());
+      const toUser = await getUser(authToken!, alias);
+
+      if (toUser) {
+        if (!toUser.equals(displayedUser!)) {
+          setDisplayedUser(toUser);
+          navigate(`${featurePath}/${toUser.alias}`);
+        }
+      }
+    } catch (error) {
+      displayErrorMessage(`Failed to get user because of exception: ${error}`);
+    }
+  };
+
+  return { navigateToUser };
+};
---
+++ tweeter-web-starter/tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { Status } from "tweeter-shared";
+import Post from "./Post";
+import { useUserNavigation } from "../mainLayout/NavigationHooks";
+
+interface Props {
+  status: Status;
+  featurePath: string;
+}
+
+const StatusItem = (props: Props) => {
+  const { navigateToUser } = useUserNavigation();
+
+  return (
+    <div className="col bg-light mx-0 px-0">
+      <div className="container px-0">
+        <div className="row mx-0 px-0">
+          <div className="col-auto p-3">
+            <img
+              src={props.status.user.imageUrl}
+              className="img-fluid"
+              width="80"
+              alt="Posting user"
+            />
+          </div>
+          <div className="col">
+            <h2>
+              <b>
+                {props.status.user.firstName} {props.status.user.lastName}
+              </b>{" "}
+              -{" "}
+              <Link
+                to={`${props.featurePath}${props.status.user.alias}`}
+                onClick={(e) => navigateToUser(e, props.featurePath)}
+              >
+                {props.status.user.alias}
+              </Link>
+            </h2>
+            {props.status.formattedDate}
+            <br />
+            <Post status={props.status} featurePath={props.featurePath} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StatusItem;
