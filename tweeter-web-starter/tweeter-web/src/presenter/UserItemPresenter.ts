2d7b1856bd745112f89f485c009eaed993ddc0c3
= <emileeforbush@gmail.com>
2026-02-26 18:18:34 -0700

    Initial view/presenter generics built - followeepresenter and followerpresenter both implemented

--- tweeter-web-starter/tweeter-web/src/presenter/UserItemPresenter.ts
+++ tweeter-web-starter/tweeter-web/src/presenter/UserItemPresenter.ts
@@ -1,24 +1,19 @@
 import { AuthToken, User } from "tweeter-shared";
 import { UserService } from "../model.service/UserService";
+import { Presenter, View } from "./Presenter";
 
-export interface UserItemView {
+export interface UserItemView extends View {
   addItems: (newItems: User[]) => void;
-  displayErrorMessage: (message: string) => void;
 }
 
-export abstract class UserItemPresenter {
-  private _view: UserItemView;
+export abstract class UserItemPresenter extends Presenter<UserItemView> {
   private _hasMoreItems: boolean = true;
   private _lastItem: User | null = null;
   private userService: UserService;
 
   protected constructor(view: UserItemView) {
-    this._view = view;
+    super(view);
     this.userService = new UserService();
-  }
-
-  protected get view(): UserItemView {
-    return this._view;
   }
 
   protected get lastItem(): User | null {
