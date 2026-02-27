a152d51d6ce91d96b37e356ea75befa9f4f76fe9
= <emileeforbush@gmail.com>
2026-02-26 18:45:39 -0700

    Replaced follow and unfollow in UserInfoPresenter with reference to FollowService follow and unfollow

--- tweeter-web-starter/tweeter-web/src/presenter/UserInfoPresenter.ts
+++ tweeter-web-starter/tweeter-web/src/presenter/UserInfoPresenter.ts
@@ -141,15 +141,7 @@ export class UserInfoPresenter {
     authToken: AuthToken,
     displayedUser: User,
   ): Promise<[followerCount: number, followeeCount: number]> {
-    // Pause so we can see the follow message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
-
-    // TODO: Call the server
-
-    const followerCount = await this.getFollowerCount(authToken, displayedUser);
-    const followeeCount = await this.getFolloweeCount(authToken, displayedUser);
-
-    return [followerCount, followeeCount];
+    return this.service.follow(authToken, displayedUser);
   }
 
   public async unfollowDisplayedUser(
@@ -189,15 +181,7 @@ export class UserInfoPresenter {
     authToken: AuthToken,
     displayedUser: User,
   ): Promise<[followerCount: number, followeeCount: number]> {
-    // Pause so we can see the unfollow message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
-
-    // TODO: Call the server
-
-    const followerCount = await this.getFollowerCount(authToken, displayedUser);
-    const followeeCount = await this.getFolloweeCount(authToken, displayedUser);
-
-    return [followerCount, followeeCount];
+    return this.service.unfollow(authToken, displayedUser);
   }
   public set IsFollower(isFollower: boolean) {
     this.isFollower = isFollower;
